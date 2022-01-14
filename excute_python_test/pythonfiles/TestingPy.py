ef4941ad41abee2e412e58527bad7a79787b0933
gw0245 <xx6677as@gmail.com>
2022-01-14 16:17:31 +0900

    개인정보 데이터 가명처리

--- excute_python_test/pythonfiles/TestingPy.py
+++ excute_python_test/pythonfiles/TestingPy.py
@@ -1,8 +1,8 @@
 from faker import Faker
 
 # 기존 데이터라고 가정
-data = [['이진우',27,'xx6677as@gmail.com','010-3098-9353','서울시 강남구 선릉로 12길 10',
-        '매니저','한국']]
+data = [['XXX',27,'XXXXXXX@XXXXXX.com','XXX-XXXX-XXXX','XX시 XX구 XXX XX XX',
+        'XX저','한국']]
 
 #faker 한국용으로 사용
 fake = Faker('ko-KR')
@@ -17,7 +17,8 @@ data[0][6] = fake.country()
 
 ### DB연동
 import pymysql
-conn = pymysql.connect(host='localhost', user='root', password='fourfive12@@', db='Privacy', charset='utf8mb4')
+### 비빌번호 지움
+conn = pymysql.connect(host='localhost', user='root', password='', db='Privacy', charset='utf8mb4')
 # Connection 으로부터 Cursor 생성
 cur = conn.cursor()
 
